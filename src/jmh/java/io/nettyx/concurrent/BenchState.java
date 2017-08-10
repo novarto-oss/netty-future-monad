43a78627e7d9ff406d1ce1ec15a52a9270b23d7c
dimitarg <dimitar.georgiev.bg@gmail.com>
2017-08-10 12:18:46 +0300

    simple synchronous map bench

--- src/jmh/java/io/nettyx/concurrent/BenchState.java
+++ src/jmh/java/io/nettyx/concurrent/BenchState.java
@@ -3,8 +3,18 @@ package io.nettyx.concurrent;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
+import java.util.function.Function;
+
 @State(Scope.Benchmark)
 public class BenchState
 {
     public String string = "hello olleh";
+    public Integer anInt = 42;
+
+    public Function<Integer, Integer> doubleItFn = x -> x*2;
+
+    public Integer doubleIt(Integer in)
+    {
+        return in * 2;
+    }
 }
--- src/jmh/java/io/nettyx/concurrent/FuturesBench.java
+++ src/jmh/java/io/nettyx/concurrent/FuturesBench.java
@@ -6,6 +6,7 @@ import io.netty.util.concurrent.Future;
 import org.openjdk.jmh.annotations.Benchmark;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class FuturesBench
 {
@@ -41,4 +42,42 @@ public class FuturesBench
         result.complete(state.string);
         return result;
     }
+
+    @Benchmark
+    public Integer simpleMapConst(BenchState state) throws ExecutionException, InterruptedException
+    {
+        Future<Integer> result = FutureMonad.map(FutureMonad.unit(state.anInt), state.doubleItFn);
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer simpleMapMethodHandle(BenchState state) throws ExecutionException, InterruptedException
+    {
+        Future<Integer> result = FutureMonad.map(FutureMonad.unit(state.anInt), state::doubleIt);
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer simpleMap(BenchState state) throws ExecutionException, InterruptedException
+    {
+        Future<Integer> result = FutureMonad.map(FutureMonad.unit(state.anInt), x -> x*2);
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer simpleMapGuava(BenchState state) throws ExecutionException, InterruptedException
+    {
+        ListenableFuture<Integer> result = FutureOpAliases.map(FutureOpAliases.unit(state.anInt), x -> x*2);
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer simpleMapStd(BenchState state) throws ExecutionException, InterruptedException
+    {
+        CompletableFuture<Integer> fut = new CompletableFuture<>();
+        fut.complete(state.anInt);
+        CompletableFuture<Integer> result = fut.thenApply(x -> x * 2);
+        return result.get();
+    }
 }
+

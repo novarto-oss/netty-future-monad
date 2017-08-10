2279c8d50e71a6f838c96fa6fdb02e0bd8071576
dimitarg <dimitar.georgiev.bg@gmail.com>
2017-08-10 16:02:10 +0300

    more synchronous benches

---
+++ src/jmh/java/io/nettyx/concurrent/CompletableFutureMonad.java
@@ -0,0 +1,26 @@
+package io.nettyx.concurrent;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+
+public class CompletableFutureMonad
+{
+    public static <A> CompletableFuture<A> unit(A value)
+    {
+        CompletableFuture<A> fut = new CompletableFuture<>();
+        fut.complete(value);
+        return fut;
+    }
+
+    public static <A, B> CompletableFuture<B> map(CompletableFuture<A> fut, Function<A, B> f)
+    {
+        return fut.thenApply(f);
+    }
+
+    public static <A, B> CompletableFuture<B> flatMap(CompletableFuture<A> fut,
+            Function<? super A, ? extends CompletionStage<B>> f)
+    {
+        return fut.thenCompose(f);
+    }
+}
--- src/jmh/java/io/nettyx/concurrent/FuturesBench.java
+++ src/jmh/java/io/nettyx/concurrent/FuturesBench.java
@@ -38,9 +38,7 @@ public class FuturesBench
     @Benchmark
     public CompletableFuture<String> stdUnit(BenchState state)
     {
-        CompletableFuture<String> result = new CompletableFuture<>();
-        result.complete(state.string);
-        return result;
+        return CompletableFutureMonad.unit(state.string);
     }
 
     @Benchmark
@@ -74,10 +72,123 @@ public class FuturesBench
     @Benchmark
     public Integer simpleMapStd(BenchState state) throws ExecutionException, InterruptedException
     {
-        CompletableFuture<Integer> fut = new CompletableFuture<>();
-        fut.complete(state.anInt);
-        CompletableFuture<Integer> result = fut.thenApply(x -> x * 2);
+        CompletableFuture<Integer> result = CompletableFutureMonad.map(CompletableFutureMonad.unit(state.anInt), x -> x * 2);
         return result.get();
     }
+
+    @Benchmark
+    public Integer mapChain(BenchState state) throws ExecutionException, InterruptedException
+    {
+        Future<Integer> result = FutureMonad.map(
+                FutureMonad.map(
+                        FutureMonad.map(
+                                FutureMonad.map(FutureMonad.unit(state.anInt), x -> x*2),
+                                x -> x+3
+                        ),
+                        x -> x-20
+                ),
+                x -> x + 100
+        );
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer mapChainGuava(BenchState state) throws ExecutionException, InterruptedException
+    {
+        ListenableFuture<Integer> result = FutureOpAliases.map(
+                FutureOpAliases.map(
+                        FutureOpAliases.map(
+                                FutureOpAliases.map(FutureOpAliases.unit(state.anInt), x -> x*2),
+                                x -> x+3
+                        ),
+                        x -> x-20
+                ),
+                x -> x + 100
+        );
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer mapChainStd(BenchState state) throws ExecutionException, InterruptedException
+    {
+        CompletableFuture<Integer> result = CompletableFutureMonad.map(
+                CompletableFutureMonad.map(
+                        CompletableFutureMonad.map(
+                                CompletableFutureMonad.map(CompletableFutureMonad.unit(state.anInt), x -> x*2),
+                                x -> x+3
+                        ),
+                        x -> x-20
+                ),
+                x -> x + 100
+        );
+        return result.get();
+    }
+
+
+    @Benchmark
+    public Integer flatMap(BenchState state) throws ExecutionException, InterruptedException
+    {
+        Future<Integer> result = FutureMonad.flatMap(
+                FutureMonad.unit(state.anInt), x -> FutureMonad.unit(x * 2));
+        return result.get();
+
+    }
+
+    @Benchmark
+    public Integer flatMapGuava(BenchState state) throws ExecutionException, InterruptedException
+    {
+        ListenableFuture<Integer> result = FutureOpAliases.bind(
+                FutureOpAliases.unit(state.anInt), x -> FutureOpAliases.unit(x * 2));
+        return result.get();
+
+    }
+
+    @Benchmark
+    public Integer flatMapStd(BenchState state) throws ExecutionException, InterruptedException
+    {
+        CompletableFuture<Integer> result = CompletableFutureMonad.flatMap(
+                CompletableFutureMonad.unit(state.anInt), x -> CompletableFutureMonad.unit(x * 2));
+        return result.get();
+
+    }
+
+    @Benchmark
+    public Integer flatMapChain(BenchState state) throws ExecutionException, InterruptedException
+    {
+        Future<Integer> result = FutureMonad.flatMap(
+                FutureMonad.flatMap(
+                        FutureMonad.flatMap(
+                                FutureMonad.map(FutureMonad.unit(state.anInt), x -> x * 2),
+                                x -> FutureMonad.unit(x - 10)),
+                        x -> FutureMonad.unit(x + 13)),
+                x -> FutureMonad.unit(x / 2));
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer flatMapChainGuava(BenchState state) throws ExecutionException, InterruptedException
+    {
+        ListenableFuture<Integer> result = FutureOpAliases.bind(
+                FutureOpAliases.bind(FutureOpAliases.bind(
+                        FutureOpAliases.map(FutureOpAliases.unit(state.anInt), x -> x * 2),
+                        x -> FutureOpAliases.unit(x - 10)),
+                        x -> FutureOpAliases.unit(x + 13)),
+                x -> FutureOpAliases.unit(x / 2));
+        return result.get();
+    }
+
+    @Benchmark
+    public Integer flatMapChainStd(BenchState state) throws ExecutionException, InterruptedException
+    {
+        CompletableFuture<Integer> result = CompletableFutureMonad.flatMap(
+                CompletableFutureMonad.flatMap(
+                        CompletableFutureMonad.flatMap(
+                                CompletableFutureMonad.map(CompletableFutureMonad.unit(state.anInt), x -> x * 2),
+                                x -> CompletableFutureMonad.unit(x - 10)),
+                        x -> CompletableFutureMonad.unit(x + 13)),
+                x -> CompletableFutureMonad.unit(x / 2));
+        return result.get();
+    }
+
 }
 
